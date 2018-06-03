2a0b2fda3647e4e9857e848669bcc1021d5818bb
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-03 04:11:02 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,25 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = []
+  languages.values do |styles, language_names|
+  binding.pry
+  end  
 end
+
+
+
+# hash = {}
+# hash[:my_key] = "my value"
+ 
+# puts hash
+
+
+# hash = {}
+# hash[:my_key] = {second_level_key: "second level value"}
+ 
+# puts hash
+# #  => 
+# {
+#   my_key: {
+#     second_level_key: "second level value"
+#   }
+# }
