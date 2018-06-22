f9a3d834355b3ce91dd5cff1eb2de851d1e2f142
github username <github email address>
2018-06-22 15:41:53 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style_name, languages_hash|
+    languages_hash.each do |name, info|
+      new_hash[name] ||= {}
+      if new_hash[name] == {}
+        new_hash[name] = info
+        new_hash[name][:style] ||= []
+      end
+      new_hash[name][:style] << style_name
+    end
+  end
+  new_hash
 end
+
+
+# { :ruby=>{:type=>"interpreted", :style=>[:oo]},
+#   :javascript=>{:type=>"interpreted", :style=>[:functional]},
+#   :python=>{:type=>"interpreted", :style=>[:oo]},
+#   :java=>{:type=>"compiled", :style=>[:oo]},
+#   :clojure=>{:type=>"compiled", :style=>[:functional]},
+#   :erlang=>{:type=>"compiled", :style=>[:functional]},
+#   :scala=>{:type=>"compiled", :style=>[:functional]}
+# }
